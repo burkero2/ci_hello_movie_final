78c20d366f8442b44d51d38cb5d9eff3fa321f04
burkero2 <ronanburke@codeinstitute.net>
2022-06-21 15:26:38 +0000

    Enabled Automatic Deployment

--- movie_app_project/settings.py
+++ movie_app_project/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1c!%_1@k#v=g!8oo9+aab@^s$k%0osfkdld(-(98kxtq+vcut8'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'superSecretKey22')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rb-django-movie-app.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'movie_app_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        dj_databse_url.parse('postgres://tinqpjpjgwzzgi:4366b55508e2bddb6fc08db3bf0544fad22fb4e439feda87628e48eec36bdfbd@ec2-54-76-43-89.eu-west-1.compute.amazonaws.com:5432/df628rmoamc69')
+        dj_databse_url.parse(os.environ.get('DATABASE_IRL'))
     }
 }
 
