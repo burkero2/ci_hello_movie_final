66b7b4cda79f621566c7488e32b47225f4a4825b
burkero2 <ronanburke@codeinstitute.net>
2022-06-21 15:38:41 +0000

    Enabled Automatic Deployment4

--- movie_app_project/settings.py
+++ movie_app_project/settings.py
@@ -84,10 +84,9 @@ WSGI_APPLICATION = 'movie_app_project.wsgi.application'
 # }
 
 DATABASES = {
-    'default': {
-        dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
